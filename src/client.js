2e747c71b643abbbeb19ea7635454e4c537a8492
Ruben Dedecker <dedecker.ruben@gmail.com>
2020-06-01 12:45:50 +0200

    updated dependencies

--- src/client.js
+++ src/client.js
@@ -7,7 +7,7 @@ let individualRequests = new Set();
 
 // const rootURI = 'http://192.168.1.56:4000/substring/50/node0.jsonld#Collection'
 const rootURI = 'http://109.129.221.176:4000/substring/50/node0.jsonld#Collection'
-const queryTimeout = 60000;
+const queryTimeout = 20000;
 
 let acClient = new treeBrowser.FuzzyAutocompleteClient(15)
 
